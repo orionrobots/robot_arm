488caa7ac102878c5a25c5e94e0fcaa2074b2c57
Danny Staple <danny@orionrobots.co.uk>
2019-04-18 13:09:10 +0100

    update the readme and add the setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='UsbArm',
-    version='0.1dev',
+    version='0.1',
     packages=['usb_arm',],
     license='Creative Commons By Attribution Share-Alike v3.0',
     long_description=open('readme.md').read(),
