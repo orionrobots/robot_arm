d323ea77b17f8b3e79a1ebedb116c7298bd4e581
Danny Staple <danny@orionrobots.co.uk>
2019-04-18 19:33:51 +0100

    fix the heredoc and libusb dep

--- setup_arm.sh
+++ setup_arm.sh
@@ -1,7 +1,8 @@
-#!/bin/env bash
+#!/bin/env bash -e
 # Raspbian setup
-sudo apt-get install python3-libusb python3-pip
+sudo apt-get update -y 
+sudo apt-get install python3-libusb1 python3-pip
 pip3 install git+https://github.com/orionrobots/python_usb_robot_arm
-sudo cat <<END
+sudo cat <<END >/etc/udev/rules.d/42-usb-arm-permissions.rules
 SUBSYSTEM=="usb", ATTR{idVendor}=="1267", ATTR{idProduct}=="0000", MODE:="0666"
-END >/etc/udev/rules.d/42-usb-arm-permissions.rules
+END 
