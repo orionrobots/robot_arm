e9e166077293b508a20900bda76552f94b280d74
Danny Staple <danny@orionrobots.co.uk>
2019-04-18 12:52:51 +0100

    Packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='UsbArm',
+    version='0.1dev',
+    packages=['usb_arm',],
+    license='Creative Commons By Attribution Share-Alike v3.0',
+    long_description=open('readme.md').read(),
+)
---
+++ setup_arm.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+sudo apt-get install python3-libusb python3-pip
+pip3 install pyusb
+pip3 install git+https://github.com/orionrobots/python_usb_robot_arm
