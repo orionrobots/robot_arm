2e425e4c35350571373acc4add5e9facc410643b
Danny Staple <danny@orionrobots.co.uk>
2019-04-18 13:33:23 +0100

    Remove from the sh because the setup.py has it.

--- setup_arm.sh
+++ setup_arm.sh
@@ -1,7 +1,6 @@
 #!/bin/env bash
 # Raspbian setup
 sudo apt-get install python3-libusb python3-pip
-pip3 install pyusb
 pip3 install git+https://github.com/orionrobots/python_usb_robot_arm
 sudo cat <<END
 SUBSYSTEM=="usb", ATTR{idVendor}=="1267", ATTR{idProduct}=="0000", MODE:="0666"
