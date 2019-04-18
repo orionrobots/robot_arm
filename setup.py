21274ab3945c3df45a56aae77d0f622b2b186447
Danny Staple <danny@orionrobots.co.uk>
2019-04-18 13:33:02 +0100

    Add author and requires

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='usb_arm',
     version='0.1.1',
     packages=['usb_arm',],
+    author="Danny Staple - Orionrobots",
     license='Creative Commons By Attribution Share-Alike v3.0',
     long_description=open('readme.md').read(),
+    install_requires=[
+        "pyusb"
+    ]
 )
