e068ce3d66bc0fa90c4af5de3c2935f4381f206f
Danny Staple <danny@orionrobots.co.uk>
2019-04-18 19:56:02 +0100

    Heredoc unwanted char

--- setup_arm.sh
+++ setup_arm.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y python3-libusb1 python3-pip git
 pip3 install git+https://github.com/orionrobots/python_usb_robot_arm
 sudo cat <<END >/etc/udev/rules.d/42-usb-arm-permissions.rules
 SUBSYSTEM=="usb", ATTR{idVendor}=="1267", ATTR{idProduct}=="0000", MODE:="0666"
-END 
+END
